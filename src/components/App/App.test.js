5cbc632bc3a971e6d7cb6a1b7cd083e91ad1d99a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-10 10:07:31 -0700

    Add test id

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import App from './App';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+//import {   } from '../../apiCalls';
+//jest.mock('../../apiCalls');
 
+describe('App', () => {
+  it('Should pass an initial test', () => {
+    expect(true).toBe(true);
+  })
+})
--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './UrlContainer.css';
 
 const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+  const urlEls = props.urls.map((url, i) => {
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
+      <div className="url"key={i}>
+        <h3 data-testid='url-heading'>{url.title}</h3>
+        <a data-testid='short-url' href={url.short_url} target="blank">{url.short_url}</a>
+        <p data-testid='long-url'>{url.long_url}</p>
       </div>
     )
   });
