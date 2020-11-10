0f8e1082f689a20d865e246f180e57fad4ded9f4
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-10 09:41:13 -0700

    Write post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,17 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postNewURL = (newURL) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newURL)
+  })
+  .then(response => response.json())
+  // .then(data => data)
+  .then(data => console.log('success'))
+  .catch(err => console.log('failed', err.message))
+}
