5e95e074f5f5c9d0946cef9d46c27c5fcdd99d9b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-10 15:42:13 -0700

    Refactor post to use return

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,19 +3,37 @@ export const getUrls = () => {
       .then(response => response.json())
 }
 
-export const postNewURL = (newURL) => {
-  return fetch('http://localhost:3001/api/v1/urls', {
+export const postNewURL = async (newURL) => {
+  const request =  {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(newURL)
-  })
-  .then(response => response.json())
-  // .then(data => data)
-  .then(data => console.log('success'))
-  .catch(err => console.log('failed', err.message))
+  }
+  const response = await fetch('http://localhost:3001/api/v1/urls', request)
+  if (response.ok) {
+    const data = response.json()
+    return data;
+  } else {
+    throw Error(response.statusText)
+  }
 }
+
+//Previous POST:
+// export const postNewURL = (newURL) => {
+//   return fetch('http://localhost:3001/api/v1/urls', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(newURL)
+//   })
+//   .then(response => response.json())
+//   // .then(data => data)
+//   .then(data => console.log('success'))
+//   .catch(err => console.log('failed', err.message))
+// }
 
 export const deleteURL = (urlID) => {
     return fetch(`http://localhost:3001/api/v1/urls/${urlID}`, {
