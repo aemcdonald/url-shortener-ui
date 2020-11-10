838ba67e26b0f5d1d39e8117b368e2208a1d5ed4
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-10 10:08:52 -0700

    Change it block

---
+++ src/components/UrlForm/UrlForm.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlForm from './UrlForm';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+//import {  } from '../../apiCalls';
+//jest.mock('../../apiCalls');
+
+describe('UrlForm', () => {
+  it('Should pass an initial test', () => {
+    expect(true).toBe(true);
+  })
+})
